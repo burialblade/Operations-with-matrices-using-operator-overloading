6a6ecea0d5621db3634576e6dfd3d0edf36f2412
tsonidbest <paladinthetemplar@gmail.com>
2022-11-30 21:58:58 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <conio.h>
+#include "matrix.h"
+
+void Matrix::read()
+{
+    std::cout << "Please enter a size of a matrix like (3 x 3)  :\n";
+    std::cin >>u>>v;
+    std::cout << "Please enter the elements in the matrix   :\n";
+    for(int i = 0; i<u; i++)
+        for(int j = 0; j<v; j++)
+            std::cin>>array[i][j];
+}
+
+void Matrix::show()
+{
+    for(int i = 0; i < u; i++)
+        {
+            for(int j = 0; j < v; j++)
+                {
+                    std::cout << array[i][j]<<"\t";
+                }
+            std::cout << "\n";
+        }
+
+
+}
+
+int main(void){
+
+    Matrix object1, object2, object3;
+        std::cout << "  Please Enter First Matrix " << std::endl;
+            object1.read();
+
+        std::cout << "  Please Enter Second Matrix " << std::endl;
+            object2.read();
+
+            object3=object1+object2;
+        std::cout << "  Result after addition of the two matrices: " << std::endl;
+           object3.show();
+
+            object3=object1-object2;
+        std::cout << "  Result after subtraction of the two matrices: " << std::endl;
+           object3.show();
+
+            object3 = object1*object2;
+        std::cout << "  Result after multiplication of the two matrices: " << std::endl;
+            object3.show();
+
+    
+    std::cin.get();
+}
---
+++ matrix.h
@@ -0,0 +1,65 @@
+#include <iostream>
+
+class Matrix
+{
+
+private:
+            int array[10][10];
+            int u,v;
+
+public:
+            void show();       
+            Matrix operator +(Matrix);
+            Matrix operator -(Matrix);
+            Matrix operator *(Matrix);
+             void read();
+
+
+};
+
+//oveloading +
+Matrix Matrix::operator+(Matrix parameters)
+{
+        Matrix t;
+        t.u = u;
+        t.v = v;
+        std::cout << "The matrix has: "<< t.u <<" rows. "<<std::endl;
+         std::cout << "The matrix has: "<< t.v <<" columns. "<<std::endl;
+        for(int i=0;i<t.u;i++)
+                    for(int j=0;j<t.v;j++)
+                         t.array[i][j]=array[i][j]+parameters.array[i][j];
+                         return t;
+
+}
+//overloading -
+Matrix Matrix::operator-(Matrix parameters)
+{
+        Matrix t;
+        t.u = u;
+        t.v = v;
+        std::cout << "The matrix has: "<< t.u <<" rows. "<<std::endl;
+         std::cout << "The matrix has: "<< t.v <<" columns. "<<std::endl;
+        for(int i=0;i<t.u;i++)
+                    for(int j=0;j<t.v;j++)
+                         t.array[i][j]=array[i][j]-parameters.array[i][j];
+                         return t;
+
+}
+
+
+//overloading *
+Matrix Matrix::operator*(Matrix parameters)
+     {
+               Matrix t;
+               t.u=u;
+               t.v=parameters.v;
+               for(int i=0;i<t.u;i++)
+                    for(int j=0;j<t.v;j++)
+                        {
+                           t.array[i][i]=0;
+                           for(int k=0;k<v;k++)
+                                  t.array[i][j]+=array[i][k]*parameters.array[k][j];
+                        }
+                                  return t;
+       }
+
