d11c374f4bc4b560116f6fcbaa4e1e11debe4c4a
tsonidbest <paladinthetemplar@gmail.com>
2022-12-14 13:36:40 +0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -5,13 +5,13 @@ class Matrix
 
 private:
             int array[10][10];
-            int u,v;
+            int u,v;//rows and columns
 
 public:
             void show();       
-            Matrix operator +(Matrix);
-            Matrix operator -(Matrix);
-            Matrix operator *(Matrix);
+            Matrix operator+(Matrix);
+            Matrix operator-(Matrix);
+            Matrix operator*(Matrix);
              void read();
 
 
@@ -39,8 +39,8 @@ Matrix Matrix::operator-(Matrix parameters)
         t.v = v;
         std::cout << "The matrix has: "<< t.u <<" rows. "<<std::endl;
          std::cout << "The matrix has: "<< t.v <<" columns. "<<std::endl;
-        for(int i=0;i<t.u;i++)
-                    for(int j=0;j<t.v;j++)
+        for(int i = 0;i < t.u; i++)
+                    for(int j=0;j < t.v; j++)
                          t.array[i][j]=array[i][j]-parameters.array[i][j];
                          return t;
 
@@ -51,14 +51,14 @@ Matrix Matrix::operator-(Matrix parameters)
 Matrix Matrix::operator*(Matrix parameters)
      {
                Matrix t;
-               t.u=u;
-               t.v=parameters.v;
-               for(int i=0;i<t.u;i++)
-                    for(int j=0;j<t.v;j++)
+               t.u = u;
+               t.v = parameters.v;
+               for(int i=0;i < t.u; i++)
+                    for(int j=0;j < t.v; j++)
                         {
                            t.array[i][i]=0;
-                           for(int k=0;k<v;k++)
-                                  t.array[i][j]+=array[i][k]*parameters.array[k][j];
+                           for(int k=0;k < v; k++)
+                                  t.array[i][j] += array[i][k] * parameters.array[k][j];
                         }
                                   return t;
        }
